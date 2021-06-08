15dd6311a70f07f2d83763eb0364753832f2e81a
fahimmanowarj5 <fahimmanowarj5@gmail.com>
2021-06-08 16:38:52 +0600

    littol fix

--- src/UDP/UDPClient.java
+++ src/UDP/UDPClient.java
@@ -1,16 +1,47 @@
 package UDP;
 
+import javax.sound.sampled.Port;
 import java.io.*;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class UDPClient {
-    public static void main(String[] args) throws IOException {
-        DatagramSocket datagramSocket=new DatagramSocket();
-        byte[] bytes="10".getBytes();
-        InetAddress inetAddress=InetAddress.getByName("103.84.253.122");
-        DatagramPacket datagramPacket=new DatagramPacket(bytes,bytes.length,inetAddress,9806);
-        datagramSocket.send(datagramPacket);
+    private int PORT, packageSize;
+    private InetAddress IPADDRESS;
+    private DatagramSocket datagramSocket;
+
+
+    public boolean connect(String connectionString) {
+        byte[] buffer = connectionString.getBytes();
+        DatagramPacket datagramPacket = new DatagramPacket(buffer, buffer.length, IPADDRESS, PORT);
+        return send(datagramPacket);
+    }
+
+    public boolean send(DatagramPacket datagramPacket) {
+        try {
+            datagramSocket.send(datagramPacket);
+        }catch (IOException e){
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public UDPClient(String ipAddress, int port, int packageSize) throws UnknownHostException {
+        this.IPADDRESS = InetAddress.getByName(ipAddress);
+        this.PORT = port;
+        this.packageSize = packageSize;
+        try {
+            datagramSocket = new DatagramSocket();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) {
+
     }
 }
--- src/UDP/UDPServer.java
+++ src/UDP/UDPServer.java
@@ -7,10 +7,11 @@ import java.net.DatagramSocket;
 public class UDPServer {
     public static void main(String[] args) throws IOException {
         DatagramSocket datagramSocket=new DatagramSocket(9806);
-        byte[] bytes=new byte[1024];
+        byte[] bytes=new byte[32];
         DatagramPacket datagramPacket=new DatagramPacket(bytes, bytes.length);
         datagramSocket.receive(datagramPacket);
         String str=new String(datagramPacket.getData(),0, datagramPacket.getLength());
         System.out.println(str);
+
     }
 }
